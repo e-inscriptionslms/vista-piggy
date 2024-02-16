ad10db181147bd54e4c7a6fc3f1a8d4b94726e48
krishnaptl43 <=>
2024-02-16 22:13:36 +0530

    blog page half complete

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -5,59 +5,22 @@ const BlogPage = () => {
     return (
         <>
         <BreadcrumbSection/>
-            <div className="container mt-4">
-                <div className="row">
-                    <div className="col-lg-9">
-                        <h3>Lorem ipsum dolor sit.</h3>
-                        <div>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ex eaque aut deleniti,
-                                architecto consequuntur, vel maxime consectetur harum provident ipsum nemo quibusdam quas!
-                                Dolorem
-                                quaerat numquam quod repellat velit.
-                            </p>
-                        </div>
-                        <div className="overflow-hidden d-flex justify-content-center">
-                            <img src="assets/img.jpg" alt="" />
-                        </div>
-                        <div className="overflow-hidden">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, repudiandae!
-                                Provident beatae error dolores, veniam inventore neque natus eveniet, accusantium officiis
-                                in necessitatibus blanditiis consequatur doloribus vel? Laudantium, asperiores provident!
-                            </p>
-                        </div>
-
+            <div className="container mt-4 p-5">
+               <div className='row'>
+                <div className='col-9 p-3'>
+                  <div className='border bg-light p-4'>
+                    <span className='text-danger'>home </span><span>{`> technology`}</span>
+                    <h2 className='mt-3'>Lorem ipsum dolor sit amet consectetur.
+                        Lorem ipsum dolor sit amet consectetur.
+                    </h2>
+                    <div className='mt-4'>
+                    <span><i class="fa-regular fa-user"></i> VISTA PIGGY </span><span><i class="fa-regular fa-clock"></i> November 24, 2023 </span><span><i class="fa-regular fa-comment"></i> 0 comment</span>
                     </div>
-                    <div className="col-lg-3"></div>
+                    <hr />
+                  </div>
                 </div>
-
-                <div className="row">
-                    <div className="col-lg-9">
-                        <h3>Lorem ipsum dolor sit.</h3>
-                        <div>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ex eaque aut deleniti,
-                                architecto consequuntur, vel maxime consectetur harum provident ipsum nemo quibusdam quas!
-                                Dolorem
-                                quaerat numquam quod repellat velit.
-                            </p>
-                        </div>
-                        <div className="overflow-hidden d-flex justify-content-center">
-                            <img src="assets/img.jpg" alt="" />
-                        </div>
-                        <div className="overflow-hidden">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, repudiandae!
-                                Provident beatae error dolores, veniam inventore neque natus eveniet, accusantium officiis
-                                in necessitatibus blanditiis consequatur doloribus vel? Laudantium, asperiores provident!
-                            </p>
-                        </div>
-
-                    </div>
-                    <div className="col-lg-3"></div>
-                </div>
-
+                <div className='col-3'></div>
+               </div>
             </div>
 
         </>
