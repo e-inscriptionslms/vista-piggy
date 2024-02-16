187c6a4c30b7ec16dcef653fa0062040a841ccb6
akshaymalviya1997 <akshaymalviya1997@gmail.com>
2024-02-16 22:13:13 +0530

    data

--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -1,25 +1,28 @@
 import BreadcrumbSection from "../components/BreadcrumbSection";
 import BlogCateCard from "../components/blogCateCard";
 import PaginationBlogCard from "../components/paginationBlogCard";
+import { bolgdata } from "../data/bolgdata";
+
 
 export default function CategoriesPage() {
+   let cate = window.location.pathname.replace('/blog/','')
+   console.log(cate);
+   let Data = bolgdata?.filter((item)=>item.cate_name === cate).reverse()
+   console.log(Data);
     return (
         <>
             <BreadcrumbSection />
             <div className="container mt-4">
                 <div className="row d-flex justify-content-between">
-                    <BlogCateCard />
-                    <BlogCateCard />
+                    <BlogCateCard data={Data[Data.length-1]} />
+                    {Data.length>=2?<BlogCateCard data={Data[Data.length-2]} />:""}
                 </div>
             </div>
             <div className="container mb-5">
                 <div className="row">
-                    { }<PaginationBlogCard />
-                    <PaginationBlogCard />
-                    <PaginationBlogCard />
-                    <PaginationBlogCard />
-                    <PaginationBlogCard />
-                    <PaginationBlogCard />
+                    {Data?.map((item,index)=>(
+                        <PaginationBlogCard data={item} key={index}/>
+                    ))}
                 </div>
             </div>
         </>
