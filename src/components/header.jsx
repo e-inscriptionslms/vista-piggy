f5bbd47d034055c6358491b4e3cd59306bdad63f
akshaymalviya1997 <akshaymalviya1997@gmail.com>
2024-02-19 13:31:35 +0530

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Route_Path} from '../RoutePath/routepath';
+import { Route_Path } from '../RoutePath/routepath';
 
 function Header() {
     return (
         <>
-            <section className="">
+            {/* <section className="">
                 <nav className="d-flex justify-content-between bg-theme navbar">
                     <div className='ms-4'>
                         <h2 className='text-white'>logo</h2>
@@ -33,7 +33,32 @@ function Header() {
                         </div>
                     </div>
                 </nav>
-            </section>
+            </section> */}
+            <nav className="navbar navbar-expand-lg navbar-light bg-warning p-4">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Vistapiggy</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse try" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto nav_list ">
+
+                            <li className="nav-item"><Link to={Route_Path.HOME}>Home</Link></li>
+                            <li className="nav-item"><Link to={`${Route_Path.BLOG}/${Route_Path.MYTHOLOGY}`}>Mythology</Link></li>
+                            <li className="nav-item"><Link to={`${Route_Path.BLOG}/${Route_Path.TECHNOLOGY}`}>Technology</Link></li>
+                            <li className="nav-item"><Link to={`${Route_Path.BLOG}/${Route_Path.WEIRD}`}>Weird</Link></li>
+                            <li className="nav-item"><Link to={`${Route_Path.BLOG}/${Route_Path.ANIMALS}`}>Animals</Link></li>
+                            <li className="nav-item"><Link to={`${Route_Path.BLOG}/${Route_Path.ENTERTAINMENT}`}>Entertainment</Link></li>
+                            <li className="nav-item"><Link to={`${Route_Path.BLOG}/${Route_Path.HISTORY}`}>History</Link></li>
+                            <form className="d-flex">
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
